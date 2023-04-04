4af7617756a5e99b2773dbe3182e81c68f3e56bf
Jonas la Cour <jonaslacour1@gmail.com>
2023-04-04 14:28:11 +0200

    Added OpenMP benchmark

---
+++ benchmarks/omp_multicore.cc
@@ -0,0 +1,52 @@
+#include "dual.h"
+#include "util.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+int main(int argc, char** argv){   
+    int N = argc > 1 ? std::stoi(argv[1]) : 200;
+    int N_graphs = argc > 2 ? std::stoi(argv[2]) : 1000;
+    int N_runs = argc > 3 ? std::stoi(argv[3]) : 10;
+    int N_warmup = argc > 4 ? std::stoi(argv[4]) : 1;
+    int version = argc > 5 ? std::stoi(argv[5]) : 0;
+
+    if(N==22 || N%2==1 || N<20 || N>200){
+        std::cout << "N must be even and between 20 and 200 and not equal to 22." << std::endl;
+        return 1;
+    }
+
+    int Nf = N/2 + 2;
+    std::vector<node_t> in_graphs(N_graphs*Nf*6);
+    std::vector<uint8_t> in_degrees(N_graphs*Nf);
+    std::vector<node_t> out_graphs(N_graphs*N*3);
+
+    fill(in_graphs, in_degrees, Nf, N_graphs);
+
+    std::vector<double> times(N_runs); //Times in nanoseconds.
+    _V2::steady_clock::time_point start;
+    for (size_t i = 0; i < N_runs + N_warmup; i++)
+    {
+
+        if(i >= N_warmup) start = steady_clock::now();
+
+        switch (version)
+        {
+        case 0: //Shared-memory parallel version.
+            dualise_V2<6>(in_graphs, in_degrees, out_graphs, Nf, N_graphs);
+            break;
+        case 1: //Task parallel version.
+            dualise_V3<6>(in_graphs, in_degrees, out_graphs, Nf, N_graphs);
+            break;
+        default:
+            break;
+        }
+
+        if(i >= N_warmup) times[i-N_warmup] = duration<double,std::nano>(steady_clock::now() - start).count();
+    }
+    
+    std::cout << "N\t | Time\t | Time SD\t " << std::endl;
+    std::cout << N << ",  " << mean(times)/N_graphs << ", " << stddev(times)/N_graphs << std::endl;
+
+    return 0;
+}
