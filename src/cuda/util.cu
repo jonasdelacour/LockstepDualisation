07a49b3eb3224d5c6bd868d215b0f63eeac8c47e
Jonas la Cour <jonaslacour1@gmail.com>
2023-04-19 13:21:50 +0200

    Temporary path fixes

--- src/cuda/util.cu
+++ src/cuda/util.cu
@@ -2,13 +2,15 @@
 #include "dual.h"
 #include "fstream"
 #include "filesystem"
-
+#include "iostream"
 template void fill(CuArray<node_t>& G_in, CuArray<uint8_t>& degrees, const int Nf, const int N_graphs);
 
 template <typename T, typename U>
 void fill(T& G_in, U& degrees, const int Nf, const int N_graphs) {
     int N = (Nf - 2)*2;
-    const std::string path = "../isomerspace_samples/dual_layout_" + std::to_string(N) + "_seed_42";
+    std::filesystem::path p = std::filesystem::current_path();
+
+    const std::string path = p.string() + "/isomerspace_samples/dual_layout_" + std::to_string(N) + "_seed_42";
     std::ifstream samples(path, std::ios::binary);        //Open the file containing the samples.
     int fsize = std::filesystem::file_size(path);         //Get the size of the file in bytes.
     int n_samples = fsize / (Nf * 6 * sizeof(uint16_t));   //All the graphs are fullerene graphs stored in 16bit unsigned integers.
