260b343a0b56c65d465eeceeb52a2f42b33005b6
Carl Johnsen <carl-johannes@di.ku.dk>
2023-04-21 12:03:16 +0200

    Added initialization print and adjusted default parameters

--- benchmarks/baseline.cc
+++ benchmarks/baseline.cc
@@ -57,14 +57,17 @@ int main(int argc, char** argv) {
     int N =        argc > 1 ? std::stoi(argv[1]) : 200;
     int N_graphs = argc > 2 ? std::stoi(argv[2]) : 1000;
     int N_runs =   argc > 3 ? std::stoi(argv[3]) : 10;
-    int N_warmup = argc > 4 ? std::stoi(argv[4]) : 1;
+    int N_warmup = argc > 4 ? std::stoi(argv[4]) : 5;
     int version =  argc > 5 ? std::stoi(argv[5]) : 0;
     std::string filename = argc > 6 ? argv[6]    : "baseline.csv";
+    std::cout << "Dualising " << N_graphs << " triangulation graphs, each with " << N
+              << " triangles, repeated " << N_runs << " times and with " << N_warmup
+              << " warmup runs." << std::endl;
 
     std::ifstream file_check(filename);
     std::ofstream file(filename, std::ios_base::app);
     //If the file is empty, write the header.
-    if(file_check.peek() == std::ifstream::traits_type::eof()) file << "N,BS,T,TSD,TD,TDSD\n"; 
+    if(file_check.peek() == std::ifstream::traits_type::eof()) file << "N,BS,T,TSD,TD,TDSD\n";
     if(N==22 || N%2==1 || N<20 || N>200){
         std::cout
             << "N must be even and between 20 and 200 and not equal to 22."
@@ -80,11 +83,11 @@ int main(int argc, char** argv) {
     FullereneDual G;
     G.neighbours = neighbours_t(Nf, std::vector<node_t>(6));
     G.N = Nf;
-    
+
     auto path = cwd() +"/isomerspace_samples/dual_layout_" + to_string(N) + "_seed_42";
     ifstream isomer_sample(path,std::ios::binary);
     auto fsize = filesize(isomer_sample);
-    
+
     std::vector<uint16_t> input_buffer(fsize/sizeof(uint16_t));
     auto available_samples = fsize / (Nf*6*sizeof(uint16_t));
     isomer_sample.read(
@@ -95,7 +98,7 @@ int main(int argc, char** argv) {
     std::vector<double> times(N_runs); //Times in nanoseconds.
     for (size_t i = 0; i < N_runs + N_warmup; i++) {
         chrono::_V2::steady_clock::time_point start = steady_clock::now();
-	
+
         for (int j = 0; j < N_graphs; ++j) {
             for (size_t k = 0; k < Nf; k++) {
                 G.neighbours[k].clear();
