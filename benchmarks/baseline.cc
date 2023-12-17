1ffa91e657990454bb993de2205e68fd9b15c8f9
James Avery <avery@diku.dk>
2023-12-17 11:32:31 +0100

    Live graphs in baseline instead of precalculated samples + massive simplification.

--- benchmarks/baseline.cc
+++ benchmarks/baseline.cc
@@ -1,8 +1,10 @@
-#include "fullerenes/polyhedron.hh"
-#include "fullerenes/isomerdb.hh"
+#include <fullerenes/polyhedron.hh>
+#include <fullerenes/isomerdb.hh>
+#include <fullerenes/buckygen-wrapper.hh>
 #include <chrono>
 #include <fstream>
 #include <unistd.h>
+#include <stdio.h>
 #include "random"
 #include "numeric"
 #define CPPBATCH
@@ -14,64 +16,42 @@ int main(int argc, char** argv) {
     int N =        argc > 1 ? std::stoi(argv[1]) : 200;
     int N_graphs = argc > 2 ? std::stoi(argv[2]) : 1000;
     int N_runs =   argc > 3 ? std::stoi(argv[3]) : 10;
-    int N_warmup = argc > 4 ? std::stoi(argv[4]) : 5;
-    int version =  argc > 5 ? std::stoi(argv[5]) : 0;
+    int N_warmup = argc > 4 ? std::stoi(argv[4]) : 1;
+
     std::string filename = argc > 6 ? argv[6]    : "baseline.csv";
-    std::cout << "Dualising " << N_graphs << " triangulation graphs, each with " << N
-              << " triangles, repeated " << N_runs << " times and with " << N_warmup
-              << " warmup runs." << std::endl;
+    printf("Dualising %d triangulation graphs, each with %d triangles, repeated %d times and with %d warmup runs.\n",
+	   N_graphs, N, N_runs, N_warmup);
 
     std::ifstream file_check(filename);
     std::ofstream file(filename, std::ios_base::app);
     //If the file is empty, write the header.
     if(file_check.peek() == std::ifstream::traits_type::eof()) file << "N,BS,T,TSD,TD,TDSD\n";
-    if(N==22 || N%2==1 || N<20 || N>200){
-        std::cout
-            << "N must be even and between 20 and 200 and not equal to 22."
-            << std::endl;
-        return 1;
+    if(N==22 || N%2==1 || N<20 || N>200) {
+        std::cout  << "N must be even and between 20 and 200 and not equal to 22.\n";
+        return -1;
+    }
+    
+    // 1. Fill buffer with graphs
+    int Nf = N/2+2; // Number of vertices in triangulation     
+    vector<FullereneDual> in_graphs(N_graphs,Triangulation(Nf));
+    auto BuckyQ = BuckyGen::start(N);
+    bool more_isomers = false;
+    int isomer_ix     = 0;
+    while((isomer_ix<N_graphs) && (more_isomers = BuckyGen::next_fullerene(BuckyQ,in_graphs[isomer_ix]))) {in_graphs[isomer_ix].update(); isomer_ix++; }
+    if(!more_isomers)		// If isomer space is too small, repeat to fill buffer.
+      for(int i=isomer_ix-1;i<N_graphs;i++) in_graphs[i] = in_graphs[i%isomer_ix];
+
+    // 2. Dualize them all and time
+    vector<double> times(N_runs,0);
+    
+    for(int i=0;i<N_warmup+N_runs;i++){
+      auto T0 = steady_clock::now();      
+      for(int j=0;j<N_graphs;j++)
+	PlanarGraph G = in_graphs[j].dual_graph();
+      auto T1 = steady_clock::now();
+      if(i >= N_warmup) times[i-N_warmup] = duration<double,std::nano>(T1-T0).count();
     }
 
-    auto sample_size = ::min<size_t>(200,IsomerDB::number_isomers(N));
-    int Nf = N/2 + 2;
-    std::vector<node_t> in_graphs(N_graphs*Nf*6);
-    std::vector<uint8_t> in_degrees(N_graphs*Nf);
-    std::vector<node_t> out_graphs(N_graphs*N*3);
-    FullereneDual G;
-    G.neighbours = neighbours_t(Nf, std::vector<node_t>(6));
-    G.N = Nf;
-
-    auto path = cwd() +"/isomerspace_samples/dual_layout_" + to_string(N) + "_seed_42";
-    ifstream isomer_sample(path,std::ios::binary);
-    auto fsize = filesize(isomer_sample);
-
-    std::vector<uint16_t> input_buffer(fsize/sizeof(uint16_t));
-    auto available_samples = fsize / (Nf*6*sizeof(uint16_t));
-    isomer_sample.read(
-        reinterpret_cast<char*>(input_buffer.data()),
-        Nf*6*sizeof(uint16_t)*available_samples);
-
-
-    std::vector<double> times(N_runs); //Times in nanoseconds.
-    for (size_t i = 0; i < N_runs + N_warmup; i++) {
-        chrono::_V2::steady_clock::time_point start = steady_clock::now();
-
-        for (int j = 0; j < N_graphs; ++j) {
-            for (size_t k = 0; k < Nf; k++) {
-                G.neighbours[k].clear();
-                for (size_t ii = 0; ii < 6; ii++) {
-                    auto u = input_buffer[(j%available_samples)*Nf*6 + k*6 +ii];
-                    if(u != UINT16_MAX) G.neighbours[k].push_back(u);
-                }
-            }
-
-            G.update();
-            PlanarGraph pG = G.dual_graph();
-        }
-        if(i >= N_warmup)
-            times[i-N_warmup] = duration<double,std::nano>(
-                steady_clock::now() - start).count();
-    }
     //Removes data points that are more than 3 standard deviations away from the mean. (Can be adjusted)
     remove_outliers(times);
 
